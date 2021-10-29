298933d98931804136d8e23a5e5bd4c438d76fb1
yishenggudou <yishenggudou@gmail.com>
2021-10-29 14:29:56 +0800

    Initial commit

---
+++ src/main/java/com/yishenggudou/camellearn/CamelLearnApplication.java
@@ -0,0 +1,13 @@
+package com.yishenggudou.camellearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamelLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yishenggudou/camellearn/CamelLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yishenggudou.camellearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
